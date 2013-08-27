7409ae37505da2b1265acb8aecb7d68f02950758
Sanderson Santana <sanderson@digaeita.com.br>
2013-08-26 23:12:15 -0300

    ahhh cielo

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -3,6 +3,30 @@ Spree::CheckoutController.class_eval do
     rescue_from ::Cielo::PaymentError, :with => :handle_cielo_error
   end
 
+  def update
+    if @order.update_attributes(object_params)
+      fire_event('spree.checkout.update')
+
+      unless @order.next
+        flash[:error] = @order.errors[:base].join("\n")
+        redirect_to checkout_state_path(@order.state) and return
+      end
+
+      if @order.completed? and @order.payments.last.payment_method.class == Spree::PaymentMethod::CieloRegularMethod && !@order.paid?
+        redirect_to @order.payment.source.authentication_url
+      elsif @order.completed?
+        session[:order_id] = nil
+        flash.notice = Spree.t(:order_processed_successfully)
+        flash[:commerce_tracking] = "nothing special"
+        redirect_to completion_route
+      else
+        redirect_to checkout_state_path(@order.state)
+      end
+    else
+      render :edit
+    end
+  end
+
   private
     def handle_cielo_error(e)
       flash[:error] = e.model_errors
